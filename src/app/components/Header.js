93fdeb436dc48984e6413783f8eb47b2271ee961
ericjiji3 <eric.ji3333@gmail.com>
2023-07-06 15:56:29 -0500

    put some assets up

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Header(){
+    return (
+        <div className="nav-container">
+            <Link href="/">Home</Link>
+            <Link href="/music">Music</Link>
+            <Link href="/video">Videos</Link>
+        </div>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <video className="bg-video" autoPlay={true} loop={true} playsInline muted width="100%">
+          <source src={require('../../public/bg-video.mp4')} type="video/mp4"></source>
+        </video>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   )
 }
