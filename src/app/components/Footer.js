37cdac2d7703c27ae98c02abdc8353cb3130a19f
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2023-07-09 19:40:00 -0500

    got supabase adding emails

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,25 +1,28 @@
+'use client'
 import Link from 'next/link'
-import { cookies } from 'next/headers'
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
-import { revalidatePath } from 'next/cache'
+import {supabase} from '../../../supabase';
+import { useState } from "react";
 
 export default function Footer(){
-    const addEmail = async (formData) => {
-        'use server'
-    
-        const email = formData.get('email')
-        const supabase = createServerActionClient({ cookies })
-        await supabase.from('emails').insert({ email })
-        revalidatePath('/')
-      }
+    const [email,setEmail] = useState('');
+    const handleChange = (e) =>{
+        setEmail(e.target.value);
+        console.log(email);
+    }
+    const addEmail = async() =>{
+        const {data,err} = await supabase.from("emails").select('email').eq('email', email)
+        console.log(data)
+        if(data.length == 0){
+            const {data2, err2} = await supabase.from("emails").insert({email: email})
+        }else{
+            console.log('non uinique email')
+        }
+    }
     return (
         <div className="footer-container">
-            <form action={addEmail}>
                 <label for="email">Stay up to date with DA$H:</label>
-                <input type="text" id="email" name="email"></input>
-                <input type="submit" value="Submit"></input>
-            </form>
-            
+                <input type="text" id="email" name="email" value={email} onChange={handleChange}></input>
+                <button onClick={addEmail}>Submit</button>
         </div>
     )
 }
