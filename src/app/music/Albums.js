ee3bab7dda8019d97f26f18f4dc0463d6135e1fe
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2023-07-13 21:02:54 -0500

    added spotify 3

--- src/app/music/Albums.js
+++ src/app/music/Albums.js
@@ -6,21 +6,24 @@ export default function Album({ albumsData }){
         <div className={styles.albumsContainer}>
             {
                 albumsData.items.map((album, index) =>{
-                    console.log(album)
-                    return(
-                        <div key={index} className={styles.albumContainer}>
-                            <a href={album.external_urls.spotify} className={styles.albumLink}>
-                                
-                                <Image
-                                    src={album.images[1].url}
-                                    width={album.images[1].width}
-                                    height={album.images[1].height}
-                                    alt="oops"
-                                />
-                                <span>{album.name}</span>
-                            </a>
-                        </div>
-                    )
+                    
+                    if(album != undefined){
+                        return(
+                        
+                            <div key={index} className={styles.albumContainer}>
+                                <a href={album.external_urls.spotify} className={styles.albumLink}>
+                                    
+                                    <Image
+                                        src={album.images[1].url}
+                                        width={album.images[1].width}
+                                        height={album.images[1].height}
+                                        alt="oops"
+                                    />
+                                    <span>{album.name}</span>
+                                </a>
+                            </div>
+                        )
+                    }
                 })
             }
         </div>
