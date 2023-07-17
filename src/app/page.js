f12a8caaaa380c0eb7cb1dab86f5f75b09b3c0d0
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2023-07-17 18:25:31 -0500

    please help

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,44 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
 import FrontAlbum from '../../public/album-front.jpeg'
 import BackAlbum from '../../public/album-back.jpeg'
-import Video from './components/Video.js';
+import React from 'react'
+import ReactPlayer from 'react-player'
+import {useState, useEffect} from 'react';
 import localFont from 'next/font/local'
 
 const sectar = localFont({src: './fonts/Sectar.otf'})
 export default function Home() {
-
+  const [video, setVideo] = useState(null);
+  useEffect(()=>{
+    setVideo(
+    <ReactPlayer
+      url="https://d1sxqb6wcb4nin.cloudfront.net/skrewface-commerical.mp4"
+      width={700}
+      height={400}
+      controls={true}
+      className={styles.videoContainer}
+      
+     />
+    )
+  }, [])
 
   return (
     <main className={styles.homeContainer}>
-      <h2 className={sectar.className}>ALBUM OUT NOW</h2>
+      <h2 className={`${sectar.className}`}>ALBUM OUT NOW</h2>
      <div className={styles.albumContainer}>
         <Image src={BackAlbum} width={300} height={300} alt="oops"/>
         <Image src={FrontAlbum} width={300} height={300} alt="oops"/>
      </div>
-     <Video urlText="https://d1sxqb6wcb4nin.cloudfront.net/skrewface-commerical.mp4"/>
+     <ReactPlayer
+      url="https://d1sxqb6wcb4nin.cloudfront.net/skrewface-commerical.mp4"
+      width={700}
+      height={400}
+      controls={true}
+      className={styles.videoContainer}
+      
+     />
     </main>
   )
 }
